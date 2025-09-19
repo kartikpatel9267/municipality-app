266dee5372dcaeae9f53842a4cf9d0918742d8e9
kartikpatel9267 <np827884@gmail.com>
2025-09-19 23:22:41 +0530

    Create firebase.js

---
+++ utils/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  databaseURL: "https://your-app.firebaseio.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "1:xxxx:web:xxxx"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
